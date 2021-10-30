37504073a7b538c82a49934406d41a8cfc5c6d1c
Tarun-mannam <85052171+Tarun-mannam@users.noreply.github.com>
2021-10-30 11:02:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var images = ['lpu1.jpg', 'lpu1.jpg', 'lpu1.jpg'];
+
+var i = 0;
+function slideShow() {
+  document.getElementById("image").src = images[i];
+  if (i < images.length - 1) {
+    i++;
+  } else {
+    i = 0;
+  }
+  setTimeout("slideShow()", 3000);
+}
